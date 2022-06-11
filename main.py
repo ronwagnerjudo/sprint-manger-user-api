2c0d550b9869e2fc0841519ab08b6a587bf98996
ronwagnerjudo <ron.wagner.judo@gmail.com>
2022-06-11 23:10:20 +0300

    changing the all the main.py code, change db name

--- main.py
+++ main.py
@@ -1,108 +1,130 @@
 import os
-import pathlib
-import requests
-from flask import Flask, render_template, session, abort, redirect, request
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
+import json
+import flask
+import jwt
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import Flow
+from google.auth.transport.requests import Request
 
+# app config
+app = flask.Flask(__name__)
+app.secret_key = os.urandom(12)
+CORS(app,  supports_credentials=True)
 
-app = Flask("Google Login App")  #naming our application
-app.secret_key = "34infojfrfjn3oi53gg"  #it is necessary to set a password when dealing with OAuth 2.0
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  #this is to set our environment to https because OAuth 2.0 only supports https environments
-
+# db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users-sprint-manager.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Users(UserMixin, db.Model):
+class UsersSprintManager(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    sub_id = db.Column(db.Integer, unique=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
+    credentials = db.Column(db.Text, unique=True)
+    sub_id = db.Column(db.String, unique=True)
 
 db.create_all()
+  
+# jwt
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 600
 
-GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]  #enter your client id you got from Google console
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")  #set the path to where the .json file you got Google console is
+# google scopes
+google_scopes = ['openid', 'https://www.googleapis.com/auth/calendar', 
+        'https://www.googleapis.com/auth/userinfo.email', 
+        'https://www.googleapis.com/auth/userinfo.profile']
 
-flow = Flow.from_client_secrets_file(  #Flow is OAuth 2.0 a class that stores all the information on how we want to authorize our users
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],  #here we are specifing what do we get after the authorization
-    redirect_uri="http://127.0.0.1:5000/callback"  #and the redirect URI is the point where the user will end up after the authorization
-)
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes}
 
-
-def login_is_required(function):  #a function to check if the user is authorized or not
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:  #authorization required
-            return abort(401)
-        else:
-            return function()
-
-    return wrapper
-
-
-@app.route("/login")  #the page where the user can login
+@app.route('/login', methods=["GET","POST"])
 def login():
-    authorization_url, state = flow.authorization_url()  #asking the flow class for the authorization (login) url
-    session["state"] = state
-    return redirect(authorization_url)
+    flow = Flow.from_client_secrets_file('./client_secrets.json', scopes=google_scopes)
+    flow.redirect_uri = flask.request.base_url + "/callback"
+    # Enable offline access so that you can refresh an access token without
+    # re-prompting the user for permission. Recommended for web server apps.
+    # Enable incremental authorization. Recommended as a best practice.
+    authorization_url, state = flow.authorization_url(access_type='offline',include_granted_scopes='true')
+    
+    # Store the state so the callback can verify the auth server response.
+    flask.session['state'] = state
+    return flask.redirect(authorization_url)
 
-
-@app.route("/callback")  #this is the page that will handle the callback process meaning process after the authorization
+@app.route('/login/callback', methods=["GET", "POST"])
 def callback():
-    flow.fetch_token(authorization_response=request.url)
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = flask.session['state']
 
-    if not session["state"] == request.args["state"]:
-        abort(500)  #state does not match!
+    flow = Flow.from_client_secrets_file('./client_secrets.json', scopes=google_scopes, state=state)
+    flow.redirect_uri = flask.url_for("callback", _external=True)
 
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = credentials_to_dict(flow.credentials) #save in database and sub id
 
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
+    token = credentials["token"]
+    userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
+    userinfo = userinfo_response.json()
+    sub_id = userinfo["sub"]
+
+    # Adding new user info (sub id and creds0 into the db, if the user still don't  exist
+    new_user = UsersSprintManager(
+        credentials = json.dumps(credentials),
+        sub_id = sub_id
     )
+    db.session.add(new_user)
+    db.session.commit()
 
-    session["google_id"] = id_info.get("sub")  #defing the results to show on the page
-    session["name"] = id_info.get("name")
-    session["email"] = id_info.get("email")
+    jwt_token = jwt.encode(sub_id, JWT_SECRET, JWT_ALGORITHM)
 
-    if not Users.query.filter_by(sub_id=session["google_id"]).first():
-        add_user = Users(
-            sub_id = session["google_id"],
-            name = session["name"],
-            email = session["email"]
-        )
-        db.session.add(add_user)
-        db.session.commit()
-    return redirect("/protected_area")  #the final page where the authorized users will end up
+    resp = flask.make_response(flask.redirect("https://127.0.0.1:3000/tasks"))
+    resp.set_cookie("creds", jwt_token)
+    return resp, flask.jsonify({credentials})
+
+@app.route('/userinfo')
+def userinfo():
+    if not flask.request.cookies.get('creds'):
+        response = flask.jsonify({"status": 401, "error": "Missing Creds"})
+        return response, 401
+    try: 
+        google_creds = jwt.decode(flask.request.cookies.get('creds'), JWT_SECRET, JWT_ALGORITHM)
+        creds = Credentials(google_creds)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                return 401
+        
+        token = google_creds["token"]
+        userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
+        userinfo = userinfo_response.json()
+
+        return flask.jsonify(user_info={"email": userinfo["email"], "picture": userinfo["picture"]}), 200
+    except jwt.ExpiredSignatureError:
+        response = flask.jsonify({"status": 401, "error": "Expried permissons!"})
+        return response, 401
+    except ValueError:
+        print(ValueError)
+        response = flask.jsonify({"status": 401, "error": "Not Valid creds!"})
+        return response, 401
 
 
-@app.route("/logout")  #the logout page and function
+@app.route("/logout")
 def logout():
-    session.clear()
-    return redirect("/")
+    resp = flask.make_response(flask.redirect("https://127.0.0.1:3000"))
+    resp.delete_cookie("creds")
+    return resp
 
-
-@app.route("/")  #the home page where the login button will be located
-def index():
-    return "Hello World <a href='/login'><button>Login</button></a>"
-
-
-@app.route("/protected_area")  #the page where only the authorized users can go to
-@login_is_required
-def protected_area():
-    return "hello bro"  #the logout button 
-
-
-if __name__ == "__main__": 
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
