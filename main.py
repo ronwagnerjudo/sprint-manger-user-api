f4ec16ab34cac9c39355122c86af73a1f8a50f44
ronwagnerjudo <ron.wagner.judo@gmail.com>
2022-07-04 20:32:13 +0300

    adding sprint dates to DB

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import json
-from pickle import TRUE
 import flask
 import jwt
+import logging
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import requests
@@ -10,6 +10,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 
+logging.basicConfig(level=logging.INFO)
 
 #--------------------------------APP CONFIG-----------------------------------------
 app = flask.Flask(__name__)
@@ -27,7 +28,8 @@ class UsersSprintManager(db.Model):
 	credentials = db.Column(db.Text, unique=True)
 	sub = db.Column(db.String(100), unique=True)
 	user_preference = db.Column(db.String(20))
-	sprint_time = db.Column(db.Integer)
+	sprint_start_date = db.Column(db.String(20))
+	sprint_end_date = db.Column(db.String(20))
 	start_work_hours = db.Column(db.Integer)
 	end_work_hours = db.Column(db.Integer)
 
@@ -56,6 +58,7 @@ def credentials_to_dict(credentials):
 #--------------------------------APP--------------------------------------------------
 @app.route('/login', methods=["GET", "POST"])
 def login():
+	logging.info("Starting flow.")
 	flow = Flow.from_client_secrets_file("./client_secret.json", scopes=google_scopes)
 	flow.redirect_uri = flask.request.base_url + "/callback"
 	# Enable offline access so that you can refresh an access token without
@@ -71,9 +74,12 @@ def login():
 def callback():
 	# Specify the state when creating the flow in the callback so that it can
 	# verified in the authorization server response.
+	logging.info("Checking state.")
 	if not flask.session["state"] == flask.request.args["state"]:
+		logging.info("No state.")
 		flask.abort(403)
 	state = flask.session["state"]
+	logging.info("Comparing state.")
 
 	flow = Flow.from_client_secrets_file("./client_secret.json", scopes=google_scopes, state=state)
 	flow.redirect_uri = flask.url_for("callback", _external=True)
@@ -81,62 +87,94 @@ def callback():
 	# Use the authorization server's response to fetch the OAuth 2.0 tokens.
 	authorization_response = flask.request.url
 	flow.fetch_token(authorization_response=authorization_response)
+	logging.info("Token fetched")
 	credentials = flow.credentials
 	credentials_dict = credentials_to_dict(credentials) 
 
 	token = credentials_dict["token"]
-	userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
+	logging.info("Init get response to google userinfo.")
+	try:
+		userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
+		logging.info("Sent get request to google userinfo.")
+	except:
+		logging.info("Problem with the response to google userinfo.")
+		return flask.jsonify({"message": "Problem with response."}), userinfo_response.status_code
 	userinfo = userinfo_response.json()
 	sub_id = userinfo["id"]
 
-	# Adding new user info (sub id and creds) into the db, if the user still don't  exist
+	# Adding new user info (sub id and creds) into the db, if the user still don't  exist.
+	logging.info("Querying db.")
 	user_exists = UsersSprintManager.query.filter_by(sub=sub_id).first()
 	if not user_exists:
+		logging.info("Adding new user to db.")
 		new_user = UsersSprintManager(
 			credentials = json.dumps(credentials_dict),
 			sub = sub_id,
 			user_preference = "",
-			sprint_time = 14,
+			sprint_start_date = "",
+			sprint_end_date = "",
 			start_work_hours = 9,
 			end_work_hours = 19
 		)
 		db.session.add(new_user)
 		db.session.commit()
+		logging.info("New user add to db.")
 
+	logging.info("Creating JWT.")
 	jwt_token = jwt.encode({"sub": sub_id}, JWT_SECRET, JWT_ALGORITHM)
 	 
 	resp = flask.make_response(flask.redirect("http://127.0.0.1:3000/tasks"))
+	logging.info("Setting cookie.")
 	resp.set_cookie("jwt", jwt_token)
 	return resp
 
 @app.route('/userinfo')
 def userinfo():
 	if not flask.request.cookies.get("jwt"):
+		logging.info("JWT not found.")
 		response = flask.jsonify({"status": 401, "error": "Missing Creds"})
 		return response, 401
-	try: 
+	try:
+		logging.info("Decoding JWT.") 
 		cookie_jwt = jwt.decode(flask.request.cookies.get("jwt"), JWT_SECRET, JWT_ALGORITHM)
+		logging.info("JWT decoded.")
+		logging.info("Finding user acording to the JWT.")
 		user = UsersSprintManager.query.filter_by(sub=cookie_jwt["sub"]).first()
-		credentials = json.loads(user.credentials)
-		creds = Credentials.from_authorized_user_info(credentials, google_scopes)
-		if not creds or not creds.valid:
-			if creds and creds.expired and creds.refresh_token:
-				creds.refresh(Request())
-				credentials = credentials_to_dict(creds)
-				refreshed_creds_text = json.dumps(credentials)
-				current_user = cookie_jwt["sub"]
-				creds_to_update = UsersSprintManager.query.filter_by(sub=current_user).first()
-				creds_to_update.credentials = refreshed_creds_text
-				db.session.commit()
-
-			else:
-				return 401
+		if user:
+			logging.info("User found in the DB.")
+			credentials = json.loads(user.credentials)
+			creds = Credentials.from_authorized_user_info(credentials, google_scopes)
+			if not creds or not creds.valid:
+				if creds and creds.expired and creds.refresh_token:
+					logging.info("Creds expired.")
+					creds.refresh(Request())
+					logging.info("Creds refreshed.")
+					credentials = credentials_to_dict(creds)
+					refreshed_creds_text = json.dumps(credentials)
+					current_user = cookie_jwt["sub"]
+					logging.info("Searching user in DB.")
+					creds_to_update = UsersSprintManager.query.filter_by(sub=current_user).first()
+					creds_to_update.credentials = refreshed_creds_text
+					db.session.commit()
+					logging.info("Updated new creds.")
+				else:
+					return flask.jsonify({"error": "No creds/creds not expired/no refresh token."}), 401
+		else:
+			logging.info("User was not found in DB.")
+			return flask.jsonify({"message": "Sorry, user was not found in the DB."}), 404
 
 		token = credentials["token"]
-		userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
-		userinfo = userinfo_response.json()
+		logging.info("Init get response to google userinfo.")
+		try:
+			userinfo_response = requests.get(f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={token}")
+			logging.info("Sent get request to google userinfo.")
+		except:
+			logging.info("Problem with the response to google userinfo.")
+			return flask.jsonify({"message": "Problem with response."}), userinfo_response.status_code
 
+		userinfo = userinfo_response.json()
 		return flask.jsonify({"email": userinfo["email"], "picture": userinfo["picture"]}), 200
+
 	except jwt.ExpiredSignatureError:
 		response = flask.jsonify({"status": 401, "error": "Expried permissons!"})
 		return response, 401
@@ -148,27 +186,42 @@ def userinfo():
 @app.route('/user-settings', methods=["GET", "POST"])
 def user_settings():
 	if not flask.request.cookies.get("jwt"):
+		logging.info("JWT not found.")
 		response = flask.jsonify({"status": 401, "error": "Missing Creds"})
 		return response, 401
 	try:
+		logging.info("Decoding JWT.")
 		cookie_jwt = jwt.decode(flask.request.cookies.get("jwt"), JWT_SECRET, JWT_ALGORITHM)
-		
 		if cookie_jwt:
+			logging.info("JWT decoded.")
 			sub = cookie_jwt["sub"]
+			logging.info("Searching user by JWT.")
 			user = UsersSprintManager.query.filter_by(sub=sub).first()
 			if user:
-				user_preference = flask.request.form.get("user_preference")
-				sprint_time = flask.request.form.get("sprint_time")
-				start_work_hours = flask.request.form.get("start_work_hours")
-				end_work_hours = flask.request.form.get("end_work_hours")
+				logging.info("User found.")
+				data = json.loads(flask.request.data)
+				user_preference = data["user_preference"]
+				sprint_start_date = data["sprint_start_date"]
+				sprint_end_date = data["sprint_end_date"]
+				start_work_hours = data["start_work_hours"]
+				end_work_hours = data["end_work_hours"]
+				logging.info("Getting data from the front-end")
 
 				user.user_preference = user_preference
-				user.sprint_time = sprint_time
+				user.sprint_start_time = sprint_start_date
+				user.sprint_end_time = sprint_end_date
 				user.start_work_hours = start_work_hours
 				user.end_work_hours = end_work_hours
 				db.session.commit()
-
+				logging.info("Changed user settings in the DB.")
 				return flask.jsonify({"message": "User settings changed"}), 200
+
+			else:
+				logging.info("User not found in DB.")
+				return flask.jsonify({"error": "User not found in DB"}), 404
+		else:
+			logging.info("JWT token not valid.")
+			return flask.jsonify({"error": "Not valid token."}), 403
 
 	except jwt.ExpiredSignatureError:
 		response = flask.jsonify({"status": 401, "error": "Expried permissons!"})
@@ -181,28 +234,34 @@ def user_settings():
 @app.route('/get-user-details', methods=["GET", "POST"])
 def get_user_details():
 	if not flask.request.cookies.get("jwt"):
+		logging.info("JWT not found.")
 		response = flask.jsonify({"status": 401, "error": "Missing Creds"})
 		return response, 401
 
 	try:
+		logging.info("Decoding JWT.")
 		cookie_jwt = jwt.decode(flask.request.cookies.get("jwt"), JWT_SECRET, JWT_ALGORITHM)
-		
 		if cookie_jwt:
+			logging.info("JWT decoded.")
 			sub = cookie_jwt["sub"]
+			logging.info("Searching user by JWT.")
 			user = UsersSprintManager.query.filter_by(sub=sub).first()
 			if user:
+				logging.info("User found.")
 				user_creds_json = json.loads(user.credentials)
 				user_preference = user.user_preference
-				user_sprint_time = user.sprint_time
+				user_sprint_start_date = user.sprint_start_date
+				user_sprint_end_date = user.sprint_end_date
 				user_start_work_hours = user.start_work_hours
 				user_end_work_hours = user.end_work_hours
 				return flask.jsonify(user_details={"sub": sub, "userCredentials": user_creds_json, "userPreference": user_preference,
-				 "userSprintTime": user_sprint_time, "userStartWorkHours": user_start_work_hours, "userEndWorkHours": user_end_work_hours}), 200
+				 "userSprintStartDate": user_sprint_start_date, "userSprintEndtDate": user_sprint_end_date, "userStartWorkHours": user_start_work_hours, "userEndWorkHours": user_end_work_hours}), 200
 			else:
-				return flask.jsonify({"error": "User credntials not found"}), 404
+				logging.info("User not found in DB.")
+				return flask.jsonify({"error": "User not found in DB"}), 404
 		else:
+			logging.info("JWT token not valid.")
 			return flask.jsonify({"error": "Not valid token"}), 403
-
 
 	except jwt.ExpiredSignatureError:
 		response = flask.jsonify({"status": 401, "error": "Expried permissons!"})
@@ -217,6 +276,7 @@ def get_user_details():
 def logout():
 	resp = flask.make_response(flask.redirect("http://127.0.0.1:3000"))
 	resp.delete_cookie("jwt")
+	logging.info("User loged out.")
 	return resp
 
 if __name__ == '__main__':
